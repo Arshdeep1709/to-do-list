1d91e8ae9ee9ad5d4e78d703c78d08016b50ac3a
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2025-12-11 20:32:05 +0530

    step 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 
 const App = () => {
+  const [task, setTask] = useState("")
+  const [description, setDescription] = useState("")
+  const formSubmit = (e) => {
+    e.preventDefault()
+    console.log(task)
+    console.log(description)
+    setTask("")
+    setDescription("")
+  }
   return(
     <>
     <h3>My To-do list</h3>
-    <form>
-      <input type="text" placeholder="Enter task"/>
-      <textarea name="" rows={1} placeholder="Enter description"></textarea>
+    <form onSubmit={formSubmit}>
+      <input type="text" placeholder="Enter task" value={task} onChange={(e)=>{setTask(e.target.value)}}/>
+      <textarea name="" rows={1} placeholder="Enter description" value={description} onChange={(e)=>{setDescription(e.target.value)}}></textarea>
       <button>Add</button>
     </form>
     </>
